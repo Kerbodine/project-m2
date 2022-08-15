2b0c6c5c110edccd40b8b2904f407521315241b9
Michael Tong <michaelyuyangtong@gmail.com>
2022-08-15 22:18:19 +0800

    Added main layout

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="mx-auto max-w-5xl w-full h-16 flex gap-4 items-center">
+      <p className="font-semibold tracking-tight text-xl px-3 py-1.5 hover:bg-gray-800 rounded-lg transition-colors cursor-pointer">
+        Project M²
+      </p>
+    </div>
+  );
+}
